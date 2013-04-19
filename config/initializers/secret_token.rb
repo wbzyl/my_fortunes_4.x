655269657b66715759452d77c0f534fc61e9494d
Wlodek Bzyl <matwb@univ.gda.pl>
2013-04-19 23:29:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyFortunes::Application.config.secret_key_base = '17937006c34b35e8469037e270486372d86443c8b360e6cfc5dd0a6892a0b461693c92b650835809d9175d81a5e0d1a282fc2d09e8c7839bbc9424b82bd1021e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyFortunes::Application.config.session_store :encrypted_cookie_store, key: '_my_fortunes_session'
